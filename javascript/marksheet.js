0c75345077b67fe598240f6a9f300ea45ef8cc4c
Andros Fenollosa <andros@fenollosa.email>
2017-06-14 18:55:12 +0200

    Change index

--- javascript/marksheet.js
+++ javascript/marksheet.js
@@ -34,31 +34,6 @@ jQuery(document).ready(function ($) {
     $('html').removeClass('open-menu').removeClass('open-navigation');
   });
 
-  // Social
 
-  var loadSocialScripts;
-
-  loadSocialScripts = function() {
-    var script;
-    script = document.createElement('script');
-    script.async = true;
-    script.id = 'facebook-jssdk';
-    script.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=470761896424893';
-    document.body.appendChild(script);
-    script = document.createElement('script');
-    script.async = true;
-    script.id = 'twitter-wjs';
-    script.src = 'https://platform.twitter.com/widgets.js';
-    document.body.appendChild(script);
-    var callback;
-    callback = function() {
-      return $('#hello').fadeTo(1000, 1);
-    };
-    return setTimeout(callback, 500);
-  };
-
-  $(window).load(function() {
-    loadSocialScripts();
-  });
 
 });
